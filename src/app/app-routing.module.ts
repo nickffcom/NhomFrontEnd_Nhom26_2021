59c54eafb31cd2f3fd998bc8cd4498e57a55fd74
Hiep299 <85495849+Hiep299@users.noreply.github.com>
2021-09-02 20:04:19 +0700

    update gdien

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatroomComponent } from './chatroom/chatroom.component';
 import { ChatsoloComponent } from './chatsolo/chatsolo.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -7,6 +8,7 @@ const routes: Routes = [
   {path:"login",component:LoginComponent},
   {path:"register",component:RegisterComponent},
   {path:"chatsolo",component:ChatsoloComponent},
+  {path:"chatroom",component:ChatroomComponent}
 
 ];
 
