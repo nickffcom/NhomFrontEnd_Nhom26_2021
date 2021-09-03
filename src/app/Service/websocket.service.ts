8942080e8875c91d04499f709e3ec23f17ea914e
nickffcom <18130002@st.hcmuaf.edu.vn>
2021-09-03 17:03:52 +0700

    Kết nối API Và làm chức năng Đăng Ký

--- src/app/Service/websocket.service.ts
+++ src/app/Service/websocket.service.ts
@@ -12,12 +12,57 @@ export class WebsocketService {
   private ws = webSocket(this.wsUrl);
 
   constructor(private router:Router,private userService:UserService) {
-    
+    this.getMessageFromServer();
+   }
+   RegisterToServer(username:string,password:string){
+    let data={
+      "action": "onchat",
+      "data": {
+        "event": "REGISTER",
+        "data": {
+          "user": username,
+          "pass": password
+        }
+      }
+      }
+   this.ws.next(data);
    }
    
-  //historychat
-  
+   getMessageFromServer() {
+    this.ws.subscribe((msg:any)=>{
+      // let event = Object.values(msg)[0]; 
+      // bắt sự kiện từ sever mỗi khi có sự thay đổi trả về 
+      let event=msg.event;
+      console.log("Stt trả về là "+event);
+      switch(event){
+        case 'REGISTER':
+          let statusRegister=msg.status;
+         
+          this.checkRegister(statusRegister);
+         break;
+        
+        default:
+
+      }
 
     }
+    )};
+
+
+    // hàm check satus register
+    checkRegister(status:string){
+      if(status=="success"){
+        alert("Bạn đã đăng kí thành công")
+
+      }else{
+        alert("Tài khoản đã tồn tại "+status)
+
+      }
+
+    }
+
+
+    } // ngoặc kết thúc classss
+
 
 
