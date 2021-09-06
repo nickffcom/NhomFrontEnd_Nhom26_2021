5b536d7dab94dbb328c97fd13d6e30fa6f6582d8
nickffcom <18130002@st.hcmuaf.edu.vn>
2021-09-06 17:30:17 +0700

    Đăng Nhập API bằng tài khoản đã đăng ký

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,6 +13,17 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
   //login 
-  
+   login(){
+    let Iusername=document.getElementById("username") as HTMLInputElement;
+    let Ipassword=document.getElementById("password") as HTMLInputElement;
+    //
+    let username=Iusername.value;
+    let password=Ipassword.value;
+    //
+    localStorage.setItem('username',username);
+    localStorage.setItem('password',password);
+   
+    this.ws.LoginToServer(username,password);
+   }
 
 }
